ccdb5537cbf2a8da6273baf0c29948a17d4298d3
essahussein <essahusseindev@gmail.com>
2025-11-06 04:34:48 +0200

    last i hope

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const slides = document.getElementById('slides');
 const totalSlides = slides.children.length;
 var slideWidth = slides.children[0].offsetWidth;
 let index = 0;
-var vw = window.innerWidth;
-var end_point = (slideWidth * totalSlides) - vw + 39;
+var vw = document.getElementById('slider__').offsetWidth;
+var end_point = (slideWidth * totalSlides) - vw + 21;
 
 
 document.getElementById('next').addEventListener('click', () => {
     document.getElementById('prev').classList.remove('hido');
 
-    var current_index = slides.dataset.index ? parseInt(slides.dataset.index) : 0;
+    var current_index = slides.dataset.index ? parseInt(slides.dataset.index) : 0;//-671px, -692px
     if ((end_point - current_index) >= slideWidth) {//click_count + 1 < (totalSlides - 1)
         index += slideWidth;
     } else {
